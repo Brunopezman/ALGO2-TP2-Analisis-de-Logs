96827116abca035a43aaf2a6d49213cea6e5718a
agosburgo <103945113+agosburgo@users.noreply.github.com>
2024-07-06 12:51:40 -0300

    Update servidorLogs.go

--- comandos/servidorLogs.go
+++ comandos/servidorLogs.go
@@ -1,15 +1,13 @@
 package comandos
 
-type ServidorLogs interface {
-	// Procesa de forma completa un archivo de log
-	Agregar_archivo(string) error
+type DetectorLogs interface {
+	// Procesa de forma completa un archivo de log e Identifica posibles situaciones de sobrecarga de solicitudes de un mismo cliente
+	// en un tiempo breve sobre un mismo servidor.
+	AgregarArchivo(string) ([]string, error)
 
 	//muestra todas las IPs que solicitaron algún recurso en el servidor, dentro del rango de IPs determinado.
-	Ver_visitantes(desde string, hasta string) []string
+	VerVisitantes(desde string, hasta string) []string
 
 	// Muestra los n recursos más solicitados.
-	Ver_mas_visitados(n int) []parSitioVisitas
-
-	// Identifica posibles situaciones de sobrecarga de solicitudes de un mismo cliente en un tiempo breve sobre un mismo servidor.
-	DOS() []string
+	VerMasVisitados(n int) []parSitioVisitas
 }
