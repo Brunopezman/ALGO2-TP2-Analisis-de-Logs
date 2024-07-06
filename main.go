0c085150faeccdb177b047ddf69f9708691229e5
agosburgo <103945113+agosburgo@users.noreply.github.com>
2024-07-06 12:49:17 -0300

    Update main.go

--- main.go
+++ main.go
@@ -23,22 +23,22 @@ func main() {
 		expresion := scanner.Text()
 		elementos := strings.Fields(expresion)
 		comando := elementos[0]
+
 		switch comando {
 		case _AGREGAR_ARCHIVO:
 			if len(elementos) != 2 {
 				fmt.Fprintf(os.Stderr, "Error en comando %s\n", _AGREGAR_ARCHIVO)
 				return
 			}
-			err := logs.Agregar_archivo(elementos[1])
+			dos, err := logs.AgregarArchivo(elementos[1])
+
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Error en comando %s\n", _AGREGAR_ARCHIVO)
 				break
 			}
-			listaIPs := logs.DOS()
-			if len(listaIPs) != 0 {
-				for _, ip := range listaIPs {
-					fmt.Fprintf(os.Stdout, "DoS: %s\n", ip)
-				}
+
+			for _, ip := range dos {
+				fmt.Fprintf(os.Stdout, "DoS: %s\n", ip)
 			}
 			fmt.Fprintln(os.Stdout, "OK")
 		case _VER_VISITANTES:
@@ -46,7 +46,7 @@ func main() {
 				fmt.Fprintf(os.Stderr, "Error en comando %s\n", _VER_VISITANTES)
 				break
 			}
-			visitantes := logs.Ver_visitantes(elementos[1], elementos[2])
+			visitantes := logs.VerVisitantes(elementos[1], elementos[2])
 			fmt.Fprintf(os.Stdout, "%s\n", "Visitantes:")
 			for _, visitante := range visitantes {
 				fmt.Fprintf(os.Stdout, "\t%s\n", visitante)
@@ -58,10 +58,10 @@ func main() {
 				break
 			}
 			n, _ := strconv.Atoi(elementos[1])
-			mas_visitados := logs.Ver_mas_visitados(n)
+			mas_visitados := logs.VerMasVisitados(n)
 			fmt.Fprintf(os.Stdout, "%s\n", "Sitios más visitados:")
 			for _, visitado := range mas_visitados {
-				sitio, visitas := visitado.Ver_par()
+				sitio, visitas := visitado.VerPar()
 				fmt.Fprintf(os.Stdout, "\t%s - %d\n", sitio, visitas)
 			}
 			fmt.Fprintln(os.Stdout, "OK")
